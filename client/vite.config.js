bc9aab668e6dd0b9f4ab012ba827c016d1cbd3d2
RootLito <angelitosentillas@gmail.com>
2025-10-29 10:18:23 +0800

    Debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-        port: 3000,
-        proxy: {
-            '/regions': {
-              target: 'https://psgc.rootscratch.com',  // The target URL
-              changeOrigin: true,  // Ensures the `Origin` header is updated
-              secure: false,  // Set to `false` if using self-signed certificates
-              rewrite: (path) => path.replace(/^\/regions/, '')  // Rewrite the path, if necessary
-            },
-          },
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/regions": {
+        target: "https://psgc.rootscratch.com", // The target URL
+        changeOrigin: true, // Ensures the `Origin` header is updated
+        secure: false, // Set to `false` if using self-signed certificates
+        rewrite: (path) => path.replace(/^\/regions/, ""), // Rewrite the path, if necessary
+      },
     },
+  },
 });
